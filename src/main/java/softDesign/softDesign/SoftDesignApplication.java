c789677e07d8b2454a7db585fc7690be718b4851
AugustoImotoo <121028016+AugustoImotoo@users.noreply.github.com>
2024-02-09 12:35:15 -0300

    Start Project
    Start Project

---
+++ src/main/java/softDesign/softDesign/SoftDesignApplication.java
@@ -0,0 +1,13 @@
+package softDesign.softDesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftDesignApplication.class, args);
+	}
+
+}
