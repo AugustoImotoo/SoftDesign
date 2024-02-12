fdbee7e06ee00e3a5a8ec1d280db5aa29d68afcb
AugustoImotoo <121028016+AugustoImotoo@users.noreply.github.com>
2024-02-12 06:19:02 -0300

    Criação do livro-cadastro
    Criação da aba de Livros no angular.

--- src/main/java/softDesign/softDesign/controller/LivroController.java
+++ src/main/java/softDesign/softDesign/controller/LivroController.java
@@ -14,6 +14,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/livro")
 public class LivroController {
 
--- src/main/resources/static/src/app/app-routing.module.ts
+++ src/main/resources/static/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PessoaListaComponent } from './pessoa-lista/pessoa-lista.component';
-
+import { LivroCadastroComponent } from './livro-cadastro/livro-cadastro.component';
 
 const routes: Routes = [
   { path: 'pessoas', component: PessoaListaComponent },
+  { path: 'livros', component: LivroCadastroComponent}
 ];
 
 @NgModule({
